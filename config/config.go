20e342316ec1bdfe34e2adb7b221a51edfc9938a
bean-lv <lvduanbing@126.com>
2019-03-31 00:07:49 +0800

    modify config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
+package config
 
+type Config interface {
+	LoadConfig(filename string)
+	Get(key string) (value interface{})
+	Set(key string, value interface{})
+}
+
+type myConfig struct {
+	data map[string]interface{}
+}
+
+func New() Config {
+	return &myConfig{}
+}
+
+func Default() Config {
+	config := New()
+
+	return config
+}
+
+func (config *myConfig) LoadConfig(filename string) {
+
+}
+
+func (config *myConfig) Get(key string) interface{} {
+	return config.data[key]
+}
+
+func (config *myConfig) Set(key string, value interface{}) {
+	config.data[key] = value
+}
